9a2d7ab36ccff4bdb0d8532b13613148790d5669
snlamm <shmuel.lamm@gmail.com>
2017-11-07 14:11:35 -0500

    normalize population samples and convert into a z-score

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const ztable = require('ztable')
+
+function average(numbers) {
+	let total = 0
+
+	for(const i of numbers) {
+		total += i
+	}
+
+	return (total / numbers.length)
+}
+
+module.exports = function(samples) {
+	const categories = Object.keys(samples)
+	let normalized = [ ]
+
+	for(const category of categories) {
+		const sample = samples[category]
+
+		if((typeof sample.mean !== 'number') || (typeof sample.sd !== 'number')) {
+			throw new Error('Mean and standard deviation (sd) must be of type `number`')
+		}
+
+		const data = [ ].concat(sample.sample)
+
+		// Normalize each member of the sample based on its population mean and sd
+		for(let i = 0; i < data.length; i ++) {
+			const num = data[i]
+			data[i] = (num - sample.mean) / sample.sd
+		}
+
+		normalized = normalized.concat(data)
+	}
+
+	const mean = average(normalized)
+	const standardError = 1 / Math.sqrt(normalized.length)
+	let zScore = Math.round((mean / standardError) * 1000000) / 1000000
+
+	// account for -0
+	if(zScore === 0) {
+		zScore = 0
+	}
+
+	return {
+		normalized,
+		mean,
+		standardError,
+		zScore,
+		proportion: ztable(zScore),
+		n: normalized.length
+	}
+}
---
+++ test/index.js
@@ -0,0 +1,95 @@
+const test = require('ava')
+const normalize = require('../index.js')
+const popA = {
+	name: 'popA',
+	// eslint-disable-next-line max-len
+	data: [ 20,  42,  30,  95,  66,  55,  46,  94,  27,  37,  90,  10,  22,  55,  31,  18,  26,  83,  30,  22,  2,  90,  28,  64,  94,  79,  9,  21,  56,  69,  21,  33,  18,  55,  67,  19,  43,  87,  26,  5,  5,  6,  7,  27,  7,  2,  49,  46,  63,  20,  5,  35,  27,  14,  19,  5,  0,  87,  69,  33,  48,  43,  3,  58,  7,  87,  53,  5,  51,  40,  14,  96,  91,  95,  25,  25,  79,  4,  57,  44,  84,  83,  82,  42,  9,  19,  77,  90,  81,  31,  50,  6,  87,  0,  88,  99,  90,  21,  89,  29 ],
+	mean: 43.93,
+	sd: 30.746,
+	n: 100
+}
+
+const popB = {
+	name: 'popB',
+	// eslint-disable-next-line max-len
+	data: [ 632,  606,  836,  306,  191,  773,  940,  359,  165,  454,  699,  62,  673,  957,  399,  68,  523,  847,  671,  148,  653,  579,  32,  386,  337,  275,  289,  753,  895,  843,  722,  789,  107,  39,  508,  695,  739,  311,  914,  681,  247,  362,  354,  238,  947,  758,  199,  840,  696,  991,  648,  671,  626,  5,  948,  249,  479,  532,  962,  181,  636,  42,  831,  260,  212,  89,  19,  549,  309,  253,  964,  566,  312,  313,  79,  722,  467,  78,  561,  969,  238,  324,  454,  784,  583,  604,  696,  519,  80,  868,  91,  517,  655,  307,  636,  51,  323,  971,  24,  269 ],
+	mean: 490.14,
+	sd: 290.043,
+	n: 100
+}
+
+
+const popC = {
+	name: 'popC',
+	// eslint-disable-next-line max-len
+	data: [ 25,  61,  23,  52,  23,  53,  22,  27,  53,  40,  41,  23,  14,  37,  37,  37,  15,  14,  23,  55,  13,  50,  45,  71,  14,  40,  52,  26,  42,  13,  45,  34,  27,  60,  48,  49,  55,  31,  52,  37,  53,  14,  32,  67,  43,  16,  26,  60,  57,  31,  28,  37,  50,  44,  25,  18,  14,  40,  22,  42,  29,  18,  45,  43,  57,  47,  41,  15,  21,  55,  44,  28,  41,  41,  50,  13,  29,  26,  65,  57,  47,  12,  54,  60,  17,  13,  48,  17,  45,  52,  69,  36,  53,  49,  45,  52,  10,  48,  15,  55 ],
+	mean: 37.6,
+	sd: 15.928,
+	n: 100
+}
+
+const populate = (a, b, c) => {
+	return {
+		popA: {
+			sd: popA.sd,
+			mean: popA.mean,
+			sample: popA.data.slice(a[0], a[1])
+		},
+		popB: {
+			sd: popB.sd,
+			mean: popB.mean,
+			sample: popB.data.slice(b[0], b[1])
+		},
+		popC: {
+			sd: popC.sd,
+			mean: popC.mean,
+			sample: popC.data.slice(c[0], c[1])
+		}
+	}
+}
+
+test('First 3 from each', t => {
+	const samples = populate([ 0, 3 ], [ 0, 3 ], [ 0, 3 ])
+	const result = normalize(samples)
+
+	t.is(result.zScore, 0.182759)
+	t.is(result.proportion, 0.5714)
+})
+
+test('One population as sample should have a 0.5 percentile', t => {
+	const samples = populate([ 0, 100 ], [ 0, 0 ], [ 0, 0 ])
+	const result = normalize(samples)
+
+	t.is(result.zScore, 0)
+	t.is(result.proportion, 0.5)
+})
+
+test('All populations as sample should have a 0.5 percentile', t => {
+	const samples = populate([ 0, 100 ], [ 0, 100 ], [ 0, 100 ])
+	const result = normalize(samples)
+
+	t.is(result.zScore, 0)
+	t.is(result.proportion, 0.5)
+})
+
+test('Differently sized slices', t => {
+	const samples = populate([ 4, 25 ], [ 15, 35 ], [ 3, 34 ])
+	const result = normalize(samples)
+
+	t.is(result.zScore, -0.379218)
+	t.is(result.proportion, 0.3557)
+})
+
+test('Throw if mean or sd are not numbers', t => {
+	const samples = populate([ 4, 25 ], [ 15, 35 ], [ 3, 34 ])
+	samples.popA.sd = null
+
+	const error1 = t.throws(() => normalize(samples), Error)
+	t.is(error1.message, 'Mean and standard deviation (sd) must be of type `number`')
+
+	const samples2 = populate([ 4, 25 ], [ 15, 35 ], [ 3, 34 ])
+	samples2.popC.mean = 'should fail'
+
+	const error2 = t.throws(() => normalize(samples2), Error)
+	t.is(error2.message, 'Mean and standard deviation (sd) must be of type `number`')
+})
